0f091d7c9cf511390b4578317c41a0e0a900760c
clairtonluz <clairton.c.l@gmail.com>
2014-12-26 18:55:41 -0300

    tela de login

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 var myApp = angular.module('bytecom',[]);
 
 myApp.controller('formController', ['$scope', '$http', function formController ($scope, $http) {
+	$scope.user = {"login" : "teste", "password" : "teste"};
     $scope.login = function() {
-    	$http.post("https://bytecom.herokuapp.com/services/securyte/login?usuario=clairton&senha=admin")
+    	$scope.message = null;
+    	var passwordHash = CryptoJS.SHA256($scope.user.password); 
+	    console.log("HASG = " + passwordHash);
+    	$http.post("https://bytecom.herokuapp.com/services/securyte/login?login="+$scope.user.login+"&password="+passwordHash)
         .success(function(response) {
+        	$scope.message = "Credenciais inválida"
 	        console.log(response);
         });
     };
