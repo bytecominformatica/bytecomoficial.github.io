4f1d60d3e3ddb90c7aa1a52dde02a9de0a27e997
clairtonluz <clairton.c.l@gmail.com>
2014-12-27 18:00:00 -0300

    implementando listagem das atividades

---
+++ js/app.js
@@ -0,0 +1,40 @@
+(function() {
+var app = angular.module('bytecom', [ ]);
+
+technicalStore = [{
+	name: "Fabiano Sousa",
+	email: "fabianosousa62@gmail.com",
+	tasks : [{
+		date: 1397490080837,
+		description: "This gem is WAY too expensive for its rarity value.",
+		type : "RECOLHIMENTO",
+		status: "REPROVADO",
+		createdOn: 1397490980837
+	  }, {
+		date: 1397400980837,
+		description: "This gem is WAY too expensive for its rarity value.",
+		type : "MANUTENÇÃO",
+		status: "AGUARDANDO APROVAÇÃO",
+		createdOn: 1397490980837
+	  }, {
+		date: 1097490980837,
+		description: "This gem is WAY too expensive for its rarity value.",
+		type : "INSTALAÇÃO",
+		status: "APROVADO",
+		createdOn: 1397490980837
+	  }]
+  }];
+
+app.directive('taskList', function(){
+	return {
+		restrict : 'E',
+		templateUrl : 'partial/task-list.html',
+		controller: function() {
+				this.technicals = technicalStore;
+			},
+		controllerAs : 'taskCtrl'
+	};
+  
+});
+
+})();
--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
-var myApp = angular.module('bytecom',[]);
+var myApp = angular.module('bytecom', []);
 
-myApp.controller('formController', ['$scope', '$http', function formController ($scope, $http) {
-	$scope.user = {"login" : "teste", "password" : "teste"};
-    $scope.login = function() {
-    	$scope.message = null;
-    	var passwordHash = CryptoJS.SHA256($scope.user.password); 
-	    console.log("HASG = " + passwordHash);
-    	$http.post("https://bytecom.herokuapp.com/services/securyte/login?login="+$scope.user.login+"&password="+passwordHash)
-        .success(function(response) {
-        	$scope.message = "Credenciais inválida"
-	        console.log(response);
-        });
-    };
+myApp.controller('loginController', ['$scope', '$http', function formController($scope, $http) {
+	$scope.login = function() {
+		$scope.message = null;
+		var passwordHash = CryptoJS.SHA256($scope.user.password);
+		// $http.post("https://bytecom.herokuapp.com/services/securyte/login?login="+$scope.user.login+"&password="+passwordHash)
+		$http.post("http://localhost:8080/services/securyte/login?login="+ $scope.user.login + "&password=" + passwordHash)
+		.success(function(response, status) {
+			if(status == 204) {
+				$scope.message = "Credenciais inválida"
+			}
+		})
+		;
+	};
 }]);
