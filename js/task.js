9b359b2eea2f0b6a455510b6774b660118210e32
clairtonluz <clairton.c.l@gmail.com>
2014-12-28 09:35:35 -0300

    melhorias de usabilidade

--- js/task.js
+++ js/task.js
@@ -3,7 +3,7 @@
 	var app = angular.module('task-directives', [ ]);
 
 
-	app.directive('taskList', function(){
+	app.directive('taskList', ['$filter', function($filter){
 		return {
 			restrict : 'E',
 			templateUrl : 'partial/task-list.html',
@@ -11,14 +11,16 @@
 					this.technical = technicalStore;
 					
 					this.task = {};
+					this.task.date = $filter('date')(Date.now(),'dd/MM/yyyy');
 					this.addTask = function(){
 					  this.task.createdOn = Date.now();
 					  this.technical.tasks.push(this.task);
-					  this.task = {};
+					  this.task = {date : Date.now()};
 					};
 					
 					this.editTask = function(task) {
 						this.task = task;
+						this.task.date = $filter('date')(this.task.date,'dd/MM/yyyy');
 					};
 					
 					this.removeTask = function(task) {
@@ -29,6 +31,6 @@
 			controllerAs : 'taskCtrl'
 		};
 	  
-	});
+	}]);
 	
 })();
