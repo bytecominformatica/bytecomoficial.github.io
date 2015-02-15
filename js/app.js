1204a0ae5b8b01934469b157bec21e9e5c0b6d01
clairtonluz <clairton.c.l@gmail.com>
2015-02-15 13:40:41 -0300

    correções de bus

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
-(function() {
-var app = angular.module('bytecom', ['task-directives']);
+var app = angular.module('bytecom', []);
 
-})();
+
+app.controller('taskCtrl', ['$scope', '$http', function($scope, $http) {
+	$http.get("http://bytecom.herokuapp.com/services/technicals?email=fabianosousa62@gmail.com").success(function(data) {
+    $scope.technical = data;
+    console.log('LOG = ' + data.tasks[0].description);
+  });
+
+  $scope.orderProp = 'name';
+}]);
+
